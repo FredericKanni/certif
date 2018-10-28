72b501202f254e2f936ab034b2621e432f873fa3
FREDERIC <kannif@ymail.com>
2018-10-28 15:01:52 +0400

    clique sur modifier envoie donnees carte vers formulaire modal

--- test/imports/ui/body.js
+++ test/imports/ui/body.js
@@ -3,6 +3,7 @@
 
 import '../api/Templates.js';
 import './template.js';
+import './card.js';
 
 import './body.html';
 import { Ateliers } from '../api/Ateliers.js';
@@ -92,4 +93,68 @@ target.champimage.value = '';
       return   Ateliers.find().fetch();
     },
    
+});
+
+
+
+Template.carte.events({    
+
+    'click .modif': function(event) {
+
+          //RECUPERE L OBJET QUI EST LE BUTTON MODIFIER
+    const target = event.target;
+    //RECUPERE L ID DE LA CARTE AUQUEL APPARTIENT LE BTN SUR LEQUEL ON VIENT DE VLIQUER
+    const idMembre = target.getAttribute('data-id');
+    //RECUPRE L OBJET DANS LA COLLECTION AVEC L ID  DE LA CARTE QU ON VIENT DE RECUPERER
+    const at = Ateliers.findOne({_id:idMembre});
+
+
+        // console.log( target );
+        // console.log( idMembre );
+        // console.log( at );
+        // console.log( at.nomatelier );
+
+//RECUPERE LE CHAMP NOM DU FORMULAIRE DU MODAL //CE FORMULAIRE SE SITUE DANS BODY.HTML
+const lenom = document.querySelector("#champnom");
+//RECUPERE LE CHAMP DESC
+    const ladesc = document.querySelector("#champdesc");
+//RECUPERE LE CHAMP URL
+    const ladate = document.querySelector("#champdate");
+//RECUPERE LE CHAMP NOM DU FORMULAIRE DU MODAL //CE FORMULAIRE SE SITUE DANS BODY.HTML
+const lheure = document.querySelector("#champheure");
+//RECUPERE LE CHAMP DESC
+    const laduree = document.querySelector("#champduree");
+//RECUPERE LE CHAMP URL
+    const nbrdispo = document.querySelector("#champdispo");
+//RECUPERE LE CHAMP NOM DU FORMULAIRE DU MODAL //CE FORMULAIRE SE SITUE DANS BODY.HTML
+const nbrreserve = document.querySelector("#champreserve");
+//RECUPERE LE CHAMP DESC
+    const leprix = document.querySelector("#champprix");
+//RECUPERE LE CHAMP URL
+    const limage = document.querySelector("#champimage");
+        
+
+    lenom.value= at.nomatelier;
+
+    //recupere les value du formulaire vide pour  linstant
+        console.log( lenom.value );
+        // console.log( ladesc.value );
+        // console.log( ladate.value );
+        // console.log( lheure.value );
+        // console.log( laduree.value );
+        // console.log( nbrdispo.value );
+        // console.log( nbrreserve.value );
+        // console.log( leprix.value );
+        // console.log( limage.value );
+
+
+
+    }
 })
+
+
+
+
+
+
+
