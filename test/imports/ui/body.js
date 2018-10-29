662aec9d6e10f8bbd1433bb83b4057b74db39c6d
FREDERIC <kannif@ymail.com>
2018-10-29 10:45:40 +0400

    fix click et data-dismiss

--- test/imports/ui/body.js
+++ test/imports/ui/body.js
@@ -17,17 +17,17 @@ import { Ateliers } from '../api/Ateliers.js';
 Template.body.events({    
 
 
-  //CREATION ATELIER
-//   Recupere le submit du formulaire
-    'submit .new-atelier'(event) {      // Prevent default browser form submit
+    'click .creer'(event) {   
+
+
+
+
 // empeche la page de s actualiser
 event.preventDefault();      // Get value from form element
     //recupere la target qui est l objet avec la classe new-atelier
-const target = event.target;
-    
+const target = document.querySelector("#createur");
 
-
-      
+// console.log(target);
 //recupere les informations dans les champ du formulaire modal
 const nomatelier = target.champnom.value;
 const descatelier = target.champdesc.value;      
@@ -54,7 +54,12 @@ console.log(imageatelier);
 
 
 
-        
+    
+
+
+
+
+
         Ateliers.insert({
     
             nomatelier,
@@ -68,11 +73,9 @@ console.log(imageatelier);
             imageatelier,  
             createdAt: new Date(), // current time
     
-        });      
-      
-      // Clear form
-    
-        
+        });  
+
+
      //RESET LES VALEUR DANS LE CHAMP DU MODAL
 target.champnom.value = '';
 target.champdesc.value = '';      
@@ -84,7 +87,94 @@ target.champreserve.value = '';
 target.champprix.value = '';      
 target.champimage.value = ''; 
     
-    }, 
+//     }, 
+
+
+
+
+
+
+
+
+    },
+  //CREATION ATELIER
+//   Recupere le submit du formulaire
+
+
+
+
+//     'submit .new-atelier'(event) {      // Prevent default browser form submit
+//     //  'submit .new-atelier'(event) {      //
+// // empeche la page de s actualiser
+// event.preventDefault();      // Get value from form element
+//     //recupere la target qui est l objet avec la classe new-atelier
+// const target = event.target;
+    
+
+
+      
+// //recupere les informations dans les champ du formulaire modal
+// const nomatelier = target.champnom.value;
+// const descatelier = target.champdesc.value;      
+// const dateatelier = target.champdate.value;
+// const heureatelier = target.champheure.value;
+// const dureeatelier = target.champduree.value;     
+// const dispoatelier = target.champdispo.value;
+// const reserveatelier = target.champreserve.value;
+// const prixatelier = target.champprix.value;      
+// const imageatelier = target.champimage.value; 
+       
+
+// //ecrit les valeur des champ du modal dans la console
+// console.log(nomatelier);
+// console.log(descatelier);
+// console.log(dateatelier);
+// console.log(heureatelier);
+// console.log(dureeatelier);
+// console.log(dispoatelier);
+// console.log(reserveatelier);
+// console.log(prixatelier);
+// console.log(imageatelier);
+
+
+
+
+        
+//         Ateliers.insert({
+    
+//             nomatelier,
+//             descatelier,
+//             dateatelier,
+//             heureatelier,
+//             dureeatelier,
+//             dispoatelier,
+//             reserveatelier,  
+//             prixatelier,
+//             imageatelier,  
+//             createdAt: new Date(), // current time
+    
+//         });      
+      
+//       // Clear form
+    
+        
+//      //RESET LES VALEUR DANS LE CHAMP DU MODAL
+// target.champnom.value = '';
+// target.champdesc.value = '';      
+// target.champdate.value = '';
+// target.champheure.value = '';
+// target.champduree.value = '';     
+// target.champdispo.value = '';
+// target.champreserve.value = '';
+// target.champprix.value = '';      
+// target.champimage.value = ''; 
+    
+//     }, 
+
+
+
+
+    
 
     // 'submit .remove'(event) { 
     //     event.preventDefault();
